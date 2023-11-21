c413cca4c6db6d4c6c37572f0dae6ab48a57ce68
johncamilomcconnell <johncmcconnell@gmail.com>
2023-11-20 20:20:24 -0600

    show issue details

---
+++ work-tracker-app/app/issues/new/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingNewIssuesPage = () => {
+  return (
+    <div>Loading...</div>
+  )
+}
+
+export default LoadingNewIssuesPage
--- work-tracker-app/app/issues/page.tsx
+++ work-tracker-app/app/issues/page.tsx
@@ -1,20 +1,18 @@
-// 'use client'
 import React from 'react';
 import { Button, Table } from '@radix-ui/themes';
 import Link from 'next/link';
 import prisma from '@/prisma/client';
 import IssueStatusBadge from '../components/IssueStatusBadge';
+import delay from 'delay';
+import IssueActions from './IssueActions';
 
 
 const IssuesPage = async () => {
     const issues = await prisma.issue.findMany();
+    await delay(4000);
   return (
     <div>
-        <div className='mb-5'>
-        <Button>
-            <Link href='/issues/new'>New Issue</Link>
-            </Button>
-            </div>
+    <IssueActions  />
     <Table.Root variant='surface'>
         <Table.Header>
             <Table.Row>
@@ -27,7 +25,9 @@ const IssuesPage = async () => {
             {issues.map(issue => (
                 <Table.Row key={issue.id}>
                     <Table.Cell>
+                        <Link href={`/issues/${issue.id}`}>
                         {issue.title}
+                        </Link>
                         <div className='block md:hidden'>
                         <IssueStatusBadge status={issue.status}/>
                         </div>
