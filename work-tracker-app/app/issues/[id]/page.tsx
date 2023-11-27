da8c68be9fa565d4642df6e6d9bfc4aab6298e0b
johncamilomcconnell <johncmcconnell@gmail.com>
2023-11-27 17:48:39 -0600

    add delete button

--- work-tracker-app/app/issues/[id]/page.tsx
+++ work-tracker-app/app/issues/[id]/page.tsx
@@ -1,8 +1,9 @@
 import prisma from '@/prisma/client';
-import { Grid, Box } from '@radix-ui/themes';
+import { Grid, Box, Flex } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
+import DeleteIssueButton from './DeleteIssueButton';
 
 
 interface Props {
@@ -19,12 +20,15 @@ const IssueDetailPage = async ( { params }: Props) => {
 
 
   return (
-    <Grid columns={ {initial: '1', md: '2'} } gap='5'>
-      <Box>
+    <Grid columns={ {initial: '1', sm: '5'} } gap='5'>
+      <Box className='md:col-span-4'>
         <IssueDetails issue={issue} />
       </Box>
       <Box>
-       <EditIssueButton issueId={issue.id}/>
+        <Flex direction='column' gap='4' >
+        <EditIssueButton issueId={issue.id}/>
+       <DeleteIssueButton issueId={issue.id} />
+        </Flex>
       </Box>
     </Grid>
   )
--- work-tracker-app/app/issues/_components/IssueForm.tsx
+++ work-tracker-app/app/issues/_components/IssueForm.tsx
@@ -1,7 +1,6 @@
 'use client';
 import "easymde/dist/easymde.min.css";
 import { TextField, Button, Callout, Text } from '@radix-ui/themes';
-import dynamic from 'next/dynamic';
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from "next/navigation";
@@ -12,15 +11,9 @@ import { z } from 'zod'
 import ErrorMessage from '@/app/components/ErrorMessage'
 import Spinner from '@/app/components/Spinner'
 import { Issue } from "@prisma/client";
-
-const SimpleMDE = dynamic(
-    () => import('react-simplemde-editor'),
-    { ssr: false }
-);
+import SimpleMDE from "react-simplemde-editor";
 
 type IssueFormData = z.infer<typeof issueSchema>
-
-
 
 const IssueForm = ( { issue }: { issue?: Issue}) => {
     const router = useRouter();
