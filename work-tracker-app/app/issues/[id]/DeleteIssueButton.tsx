9addfad87db2a3ea248578a0d7c9cc28f2d0b6c7
johncamilomcconnell <johncmcconnell@gmail.com>
2023-12-03 18:57:10 -0500

    add delete issue dialog

--- work-tracker-app/app/issues/[id]/DeleteIssueButton.tsx
+++ work-tracker-app/app/issues/[id]/DeleteIssueButton.tsx
@@ -1,8 +1,28 @@
-import { Button } from '@radix-ui/themes'
+'use client'
+import { AlertDialog, Button, AlertDialogTrigger, Flex } from '@radix-ui/themes'
 
 const DeleteIssueButton = ( { issueId }: { issueId: number }) => {
   return (
-    <Button color='red'>Delete Issue</Button>
+    <AlertDialog.Root>
+        <AlertDialogTrigger>
+        <Button color='red'>Delete Issue</Button>
+        </AlertDialogTrigger>
+        <AlertDialog.Content>
+            <AlertDialog.Title>
+                Confirm Deletion
+            </AlertDialog.Title>
+            <AlertDialog.Description>Are you sure you want to delete this issue? This action cannot be undone.</AlertDialog.Description>
+            <Flex mt='4' gap='3'>
+                <AlertDialog.Action>
+                    <Button>Delete Issue</Button>
+                </AlertDialog.Action>
+                <AlertDialog.Cancel>
+                    <Button variant='soft' color='gray'>Cancel</Button>
+                </AlertDialog.Cancel>
+            </Flex>
+        </AlertDialog.Content>
+    </AlertDialog.Root>
+    
   )
 }
 
