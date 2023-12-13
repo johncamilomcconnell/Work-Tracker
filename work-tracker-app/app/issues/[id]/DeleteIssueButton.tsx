1f77227c92afca764da49a859accc23bf8f25c60
johncamilomcconnell <johncmcconnell@gmail.com>
2023-12-12 20:10:34 -0500

    add delete button spinner

--- work-tracker-app/app/issues/[id]/DeleteIssueButton.tsx
+++ work-tracker-app/app/issues/[id]/DeleteIssueButton.tsx
@@ -2,14 +2,36 @@
 import { AlertDialog, Button, AlertDialogTrigger, Flex } from '@radix-ui/themes'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import loading from '@/app/issues/[id]/loading';
+import { Spinner } from '@/app/components'
 
 const DeleteIssueButton = ( { issueId }: { issueId: number }) => {
     const router = useRouter();
+    const [ error, setError ] = useState(false)
+    const [ isDeleting, setDeleting ] = useState(false)
+
+    const deleteIssue = async () => {
+            try {
+                setDeleting(true);
+                await axios.delete('/api/issues/' + issueId)
+                router.push('/issues');
+                router.refresh();
+            }
+            catch (error) {
+                setDeleting(false);
+                setError(true);
+            }
+    }
 
   return (
+    <>
     <AlertDialog.Root>
         <AlertDialogTrigger>
-        <Button color='red'>Delete Issue</Button>
+        <Button color='red' disabled={isDeleting}>
+            {isDeleting && <Spinner />}
+            Delete Issue
+            </Button>
         </AlertDialogTrigger>
         <AlertDialog.Content>
             <AlertDialog.Title>
@@ -18,11 +40,7 @@ const DeleteIssueButton = ( { issueId }: { issueId: number }) => {
             <AlertDialog.Description>Are you sure you want to delete this issue? This action cannot be undone.</AlertDialog.Description>
             <Flex mt='4' gap='3'>
                 <AlertDialog.Action>
-                    <Button color='red' onClick={async () => {
-                        await axios.delete('/api/issues/' + issueId)
-                        router.push('/issues')
-                        router.refresh();
-                    }}>Delete Issue</Button>
+                    <Button color='red' onClick={deleteIssue}>Delete Issue</Button>
                 </AlertDialog.Action>
                 <AlertDialog.Cancel>
                     <Button variant='soft' color='gray'>Cancel</Button>
@@ -30,7 +48,16 @@ const DeleteIssueButton = ( { issueId }: { issueId: number }) => {
             </Flex>
         </AlertDialog.Content>
     </AlertDialog.Root>
-    
+    <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+            <AlertDialog.Title>Error</AlertDialog.Title>
+            <AlertDialog.Description>This issue could not be deleted.</AlertDialog.Description>
+
+            <Button color='gray' variant='soft' mt='4' onClick={() => setError(false)}>OK</Button>
+
+        </AlertDialog.Content>
+    </AlertDialog.Root>
+    </>
   )
 }
 
